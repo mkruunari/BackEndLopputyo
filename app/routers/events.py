0b747c097399720fe5468a41c2649e760e4a4e37
Mikael Kruunari <mikael.kruunari@gmail.com>
2023-04-29 14:50:50 +0300

    Poistettu vanhat turhat koodit kommenteista

--- app/routers/events.py
+++ app/routers/events.py
@@ -3,10 +3,10 @@ from datetime import datetime
 from fastapi import APIRouter
 from typing import List
 from fastapi import HTTPException
-#from .models import Player, PlayerCreate, PlayerInDB
 from app.routers.players import players
 router = APIRouter()
 
+#tehdään Event luokka, jossa tarvittavat elementit
 class Event:
     def __init__(self, id: int, type: str, detail: str, player_id: int):
         self.id = id
@@ -41,55 +41,3 @@ async def get_events(type: Optional[str] = None):
 
 
 
-# @router.get("/events")
-# async def get_events(type: Optional[str] = None):
-#     if type and type not in ["level_started", "level_solved"]:
-#         raise HTTPException(status_code=400, detail="Unknown event type")
-#     filtered_events = [{"id": event.id, "type": event.type, "detail": event.detail, "timestamp": event.timestamp, "player_id": event.player_id} for event in events_db if not type or event.type == type]
-#     if not filtered_events:
-#         return []
-#     return filtered_events
-
-
-# @router.get("/events")
-# async def get_all_events(type: Optional[str] = None):
-#     # Suodatetaan eventit tyypin perusteella, jos type-parametri on annettu
-#     if type and type not in ["level_started", "level_solved"]:
-#         raise HTTPException(status_code=400, detail="Unknown event type")
-#     all_events = []
-#     for player in players:
-#         all_events.extend(player.events)
-#     if type:
-#         filtered_events = [event for event in all_events if event["type"] == type]
-#         return filtered_events
-#     return all_events
-
-
-
-# @router.get("/events")
-# async def get_events(type: Optional[str] = None):
-#     if type and type not in ["level_started", "level_solved"]:
-#         raise HTTPException(status_code=400, detail="Bad Request")
-#     filtered_events = [{"id": event.id, "type": event.type, "detail": event.detail, "timestamp": event.timestamp, "player_id": event.player_id} for event in events_db if not type or event.type == type]
-#     if not filtered_events:
-#         return []
-#     return filtered_events
-
-# @router.get("/events")
-# async def get_events(type: Optional[str] = None):
-#     if type:
-#         if type not in ["level_started", "level_solved"]:
-#             return {"error": "Bad Request"}, 400
-#         filtered_events = [{"id": event.id, "type": event.type, "detail": event.detail, "timestamp": event.timestamp, "player_id": event.player_id} for event in events_db if event.type == type]
-#         return filtered_events
-#     else:
-#         return [{"id": event.id, "type": event.type, "detail": event.detail, "timestamp": event.timestamp, "player_id": event.player_id} for event in events_db]
-
-# @router.post("/events")
-# async def create_event(type: str, detail: str, player_id: int):
-#     if type not in ["level_started", "level_solved"]:
-#         return {"error": "Bad Request"}, 400
-#     id = len(events_db) + 1
-#     event = Event(id, type, detail, player_id)
-#     events_db.append(event)
-#     return {"id": id, "type": type, "detail": detail, "timestamp": event.timestamp, "player_id": player_id}
--- app/routers/players.py
+++ app/routers/players.py
@@ -13,12 +13,6 @@ async def get_players():
     # return players
     return [{"id": player.id, "name": player.name} for player in players]
 
-# @router.post("/players", status_code=201)
-# async def create_player(name: str):
-#     id = len(players) + 1
-#     player = Player(id, name)
-#     players.append(player)
-#     return {"id": id, "name": name}
 
 
 @router.post("/players", status_code=201)
@@ -38,12 +32,6 @@ async def get_player(id: int):
     raise HTTPException(status_code=404, detail="Player not found")
 
 
-# @router.get("/players/{id}")
-# async def get_player(id: int):
-#     for player in players:
-#         if player.id == id:
-#             return player
-#     return {"message": "Player not found"}
 
 @router.get("/players/{id}/events")
 async def get_player_events(id: int, type: Optional[str] = None):
@@ -62,40 +50,15 @@ async def get_player_events(id: int, type: Optional[str] = None):
                         "player_id": event["player_id"]
                     })
             return filtered_events
-    # Jos pelaajaa ei löydy, palauta 404
+    # Jos pelaajaa ei löydy, palautetaan 404
     raise HTTPException(status_code=404, detail="Player not found")
-
-
-# @router.get("/players/{id}/events")
-# async def get_player_events(id: int, type: str = None):
-#     for player in players:
-#         if player.id == id:
-#             if type:
-#                 return [{"id": event["id"], "type": event["type"], "detail": event["detail"], "timestamp": event["timestamp"], "player_id": event["player_id"]} for event in player.events if event["type"] == type]
-#             else:
-#                 if player.events:
-#                     return [{"id": event["id"], "type": event["type"], "detail": event["detail"], "timestamp": event["timestamp"], "player_id": event["player_id"]} for event in player.events]
-#                 else:
-#                     return []
-#     raise HTTPException(status_code=404, detail="Player not found")
-
-
-# @router.get("/players/{id}/events")
-# async def get_player_events(id: int, type: str = None):
-#     for player in players:
-#         if player.id == id:
-#             if type:
-#                 return [{"id": event["id"], "type": event["type"], "detail": event["detail"], "timestamp": event["timestamp"], "player_id": event["player_id"]} for event in player.events if event["type"] == type]
-#             else:
-#                 return [{"id": event["id"], "type": event["type"], "detail": event["detail"], "timestamp": event["timestamp"], "player_id": event["player_id"]} for event in player.events]
-#     return {"message": "Player not found"}
 
 
 @router.post("/players/{id}/events", status_code=201)
 async def create_player_event(id: int, type: str, detail: str):
     for player in players:
         if player.id == id:
-            # Tarkista, että annettu event-tyyppi on kelvollinen
+            # Tarkistetaan, et oikea type on kelvollinen, muuten 400
             if type not in ["level_started", "level_solved"]:
                 raise HTTPException(status_code=400, detail="Unknown event type")
             # Luodaan datetime-olio nykyisestä ajasta
@@ -103,18 +66,7 @@ async def create_player_event(id: int, type: str, detail: str):
             event = {"id": len(player.events) + 1, "type": type, "detail": detail, "player_id": id, "timestamp": timestamp}
             player.events.append(event)
             return event
-    # Jos pelaajaa ei löydy, palauta 404
+     # Jos pelaajaa ei löydy, palautetaan 404
     raise HTTPException(status_code=404, detail="Player not found")
 
 
-
-# @router.post("/players/{id}/events")
-# async def create_player_event(id: int, type: str, detail: str):
-#     for player in players:
-#         if player.id == id:
-#             # Luodaan datetime-olio nykyisestä ajasta
-#             timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-#             event = {"id": len(player.events) + 1, "type": type, "detail": detail, "player_id": id, "timestamp": timestamp}
-#             player.events.append(event)
-#             return event
-#     return {"message": "Player not found"}
