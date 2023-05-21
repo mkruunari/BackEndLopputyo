a4528440f055d73c7b6640eb9188f3810b741645
Mikael Kruunari <mikael.kruunari@gmail.com>
2023-05-21 13:48:31 +0300

    endpoint kyselyjen muutos json bodyyn2

--- app/database/models.py
+++ app/database/models.py
@@ -1,19 +1,3 @@
-# from datetime import datetime
-# #Player ja Class luokat, ja niiden tarvittavat attribuutit
-# class Player:
-#     def __init__(self, id, name):
-#         self.id = id
-#         self.name = name
-#         self.events = []
-# class Event:
-#     def __init__(self, id, type, detail, player_id):
-#         self.id = id
-#         self.type = type
-#         self.detail = detail
-#         self.timestamp = datetime.now()
-#         self.player_id = player_id
-
-
 from datetime import datetime
 from typing import List
 from pydantic import BaseModel, ValidationError, validator
